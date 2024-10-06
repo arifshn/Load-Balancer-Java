3a5b1982dea339abb173989e85e319ce9b947e63
arifshn <habibarifsahin@gmail.com>
2024-10-06 22:55:54 +0300

    Load-Balancer

--- producer/src/main/java/Producer.java
+++ producer/src/main/java/Producer.java
@@ -2,7 +2,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
-
+//.
 import java.util.Properties;
 
 public class Producer {
